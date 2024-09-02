80f6ef69879932ba95659e89804a8c49db62f6af
Miraç L. Gülgönül <mlg556@gmail.com>
2024-09-02 13:21:30 +0300

    init

---
+++ client_test.py
@@ -0,0 +1,23 @@
+import sys
+
+from asyncua.sync import Client
+
+sys.path.insert(0, "../..")
+
+with Client("opc.tcp://localhost:4840/freeopcua/server/") as client:
+    # client = Client("opc.tcp://admin@localhost:4840/freeopcua/server/") #connect using a user
+    # Client has a few methods to get proxy to UA nodes that should always be in address space such as Root or Objects
+    # Node objects have methods to read and write node attributes as well as browse or populate address space
+    # print("Children of root are: ", client.nodes.root.get_children())
+
+    test_var = client.nodes.root.get_child(["0:Objects", "2:TestObject", "2:TestVariable"])
+    # print(test_var)
+    print(f"old value: {test_var.read_value()}")
+    test_var.write_value(99)
+    print(f"new value: {test_var.read_value()}")
+    test_var.write_value(42)
+    print(f"reset to: {test_var.read_value()}")
+
+
+
+
---
+++ server_test.py
@@ -0,0 +1,34 @@
+# first run server_test.py
+# while running, run client_test.py
+
+import sys
+import time
+
+from asyncua.sync import Server
+
+sys.path.insert(0, "../..")
+
+server = Server()
+server.set_endpoint("opc.tcp://0.0.0.0:4840/freeopcua/server/")
+
+# set up our own namespace, not really necessary but should as spec
+uri = "http://examples.freeopcua.github.io"
+idx = server.register_namespace(uri)
+
+test_obj = server.nodes.objects.add_object(idx, "TestObject")
+test_var = test_obj.add_variable(idx, "TestVariable", 42)
+test_var.set_writable()
+
+# starting!
+server.start()
+
+print("Server running...")
+
+try:
+    while True:
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    server.stop()
+    print("EXIT")
+
+
